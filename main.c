1c400a20d9775464fa879eec96b600614ecc1ca3
ASiKS-Engineering <35295944+ASiKS-Engineering@users.noreply.github.com>
2026-05-26 10:51:03 +0200

    Update main.c

--- main.c
+++ main.c
@@ -4,209 +4,21 @@
 #endif
 
 #include <avr/io.h>
-#include <avr/eeprom.h>
-#include <util/crc16.h>
 #include "uart.h"
 
 #include <stdlib.h>
 #include <avr/interrupt.h>
-#include <avr/signal.h>
-#include <avr/pgmspace.h>
-
-#include "helpers.h"
-
 
 //-----------------------------------------------------------------------------------------
 //		Global types and definitions
 //-----------------------------------------------------------------------------------------
 
-#define DEFAULT_COM1_BAUDRATE	10400
-#define DEFAULT_COM2_BAUDRATE	19200
-
-
-typedef union{
-uint32_t ulBaudrate;
-struct{
-uint8_t ucBaudrateByte4;
-uint8_t ucBaudrateByte3;
-uint8_t ucBaudrateByte2;
-uint8_t ucBaudrateByte1;
-}byte;
-} BAUDRATE_UNION;
-
 
 //-----------------------------------------------------------------------------------------
 //		Global parameters
 //-----------------------------------------------------------------------------------------
-BAUDRATE_UNION Baudrate_COM1;
-BAUDRATE_UNION Baudrate_COM2;
-
-
-volatile uint8_t led0_timer=0;
-volatile uint8_t led1_timer=0;
-
-char cCfgStringBuffer[100];
-
-unsigned char cPortValue; 
-
-
-
-void ClearRedLED(void)
-{
-
-		PORTA&= ~0x01;
-
-}
-
-
-
-void SetRedLED(void)
-{
-
-	PORTA |= 0x01;
-
-}
-
-
-void ClearGreenLED(void)
-{
-
-	PORTA &= ~0x02;
-
-}
-
-
-
-void SetGreenLED(void)
-{
-
-	PORTA |= 0x02;
-
-}
-
-
-uint8_t pa2_is_high(void)
-{
-    return (PINA & (1 << PA2));
-}
-
-
-
-
-//-----------------------------------------------------------------------------------------
-//		ServiceBaudrateCfg
-//-----------------------------------------------------------------------------------------
-//
-//	Description:	This routine services the baud rate configuration change mechanism
-//
-//	Parameters:		none
-//
-//	Return:			none
-//
-// 	Author:			Dr. Andreas Schramm
-//
-//	Date;Changes:	2011-07-19; Initial   	
-//
-//-----------------------------------------------------------------------------------------
-//void ServiceBaudrateCfg(void)
-ISR(PCINT0_vect)
-
-{
-
-	cPortValue = PINA & _BV(PA2);
-
-	if(cPortValue == 0x04)
-	{
-
-		if(uiCfgFlagArmed == 1)
-		{
-			// Processe received BAUDRATE config string  
-			//uart1_gets()
-		}
-		uiCfgFlagArmed = 0;
-
-		ClearRedLED();
-//		PORTA &= ~0x01;
-	}
-	else
-	{
-//		SetRedLED();
-		PORTA |= 0x01;
-
-		uiCfgFlagArmed = 1;
-
-
-	}
-
-}
-
-
-//-----------------------------------------------------------------------------------------
-//		EnableExternalInt
-//-----------------------------------------------------------------------------------------
-//
-//	Description:	This routine initialises external interrupr for PCINT2
-//
-//	Parameters:		none
-//
-//	Return:			none
-//
-// 	Author:			Dr. Andreas Schramm
-//
-//	Date;Changes:	2011-07-20; Initial   	
-//
-//-----------------------------------------------------------------------------------------
-void EnableExternalInt(void)
-{
-	
-	// Mask the interrupt pin
-	PCMSK0 = (1<<PCINT2);
-
-	// Enable interrupt
-	GICR = (1<<PCIE0);
-
-}
-
-//-----------------------------------------------------------------------------------------
-//		Init_IOs
-//-----------------------------------------------------------------------------------------
-//
-//	Description:	This routine initialises the respective IOs
-//
-//	Parameters:		none
-//
-//	Return:			none
-//
-// 	Author:			Dr. Andreas Schramm
-//
-//	Date;Changes:	2011-07-19; Initial   	
-//
-//-----------------------------------------------------------------------------------------
-void Init_IOs(void)
-{
-
-	// PA0 und PA1 als Ausgang
-	DDRA |= (1<<PA1) | (1<<PA0);//0x03; 
-
-    // PA2 als Eingang
-    DDRA &= ~(1 << PA2);
-
-	// Set internal Pullups
-	//PORTA |= (1<<PA2) | (1<<PA1) | (1<<PA0);
-	PORTA |= (1<<PA1) | (1<<PA0);
-
-	// Set PortD Pin 0 as Input
-	DDRD &= ~(1<<PD0);
-	// Set internal Pullups
-	PORTD |= (1<<PD0) | (1<<PD1);
-
-
-    // PD5 zunächst als Eingang (Tristate)
-    DDRD &= ~(1 << PD5);
-    PORTD &= ~(1 << PD5);
-
-
-}
+volatile uint8_t led0_timer = 0;
+volatile uint8_t led1_timer = 0;
 
 
 //-----------------------------------------------------------------------------------------
@@ -224,43 +36,6 @@ void Init_IOs(void)
 //	Date;Changes:	2011-07-19; Initial   	
 //
 //-----------------------------------------------------------------------------------------
-
-
-
-//-----------------------------------------------------------------------------------------
-//		InitBlinkingTimer
-//-----------------------------------------------------------------------------------------
-//
-//	Description:	This routine initialises Timer0 that serves as blinking timer for the 
-//					leds
-//
-//	Parameters:		none
-//
-//	Return:			none
-//
-// 	Author:			Dr. Andreas Schramm
-//
-//	Date;Changes:	2011-07-19; Initial   	
-//
-//-----------------------------------------------------------------------------------------
-void Timer0_Init(void)
-{
-    // 16MHz /64 = 250kHz 250 Takte =1ms OCR0=249
-
-    TCCR0=
-        (1<<WGM01)|   // CTC
-        (1<<CS01) |
-        (1<<CS00);    // /64
-
-    OCR0=249;
-
-    TIMSK |=
-        (1<<OCIE0);
-}
-
-/****************************************
- Timer Interrupt
-****************************************/
 ISR(TIMER0_COMP_vect)
 {
     if(led0_timer)
@@ -269,8 +44,7 @@ ISR(TIMER0_COMP_vect)
 
         if(!led0_timer)
         {
-            PORTA &=
-                ~(1<<PA0);
+            PORTA &= ~(1<<PA0);
         }
     }
 
@@ -280,24 +54,11 @@ ISR(TIMER0_COMP_vect)
 
         if(!led1_timer)
         {
-            PORTA &=
-                ~(1<<PA1);
+            PORTA &= ~(1<<PA1);
         }
     }
 }
 
-void UART0_Enable(void)
-{
-    UCSR0B=
-        (1<<RXEN0)|
-        (1<<TXEN0)|
-        (1<<RXCIE0);
-}
-
-void UART0_Disable(void)
-{
-    UCSR0B=0;
-}
 
 
 //-----------------------------------------------------------------------------------------
@@ -318,23 +79,52 @@ void UART0_Disable(void)
 int main (void) 
 {
 
-	uint8_t lastState=0xFF;
+	uint8_t u8_last_dtr_value=0xFF;
+	uint8_t u8_bridgeEnabled=0;
+	uint8_t u8_dtr_value = 0;
 
-	uint8_t bridgeEnabled=0;
-
-	uint8_t pa2 = 0;
-
-	// Disable all interrupts
+	uint32_t u32_Baudrate;
+	
+	/* --- Disable interrupts --- */
 	cli();
 
-	// Initialize the respective IOs
-	Init_IOs();
+	/* --- Initialize the respective IOs --- */
+	// PA0 und PA1 als Ausgang
+	DDRA |= (1<<PA1) | (1<<PA0);
 
-	ClearRedLED();
-	ClearGreenLED();
+    // PA2 als Eingang
+    DDRA &= ~(1 << PA2);
 
-	// Init timer 0 for led blinking support
-	Timer0_Init();
+	// Set internal Pullups
+	//PORTA |= (1<<PA2) | (1<<PA1) | (1<<PA0);
+	PORTA |= (1<<PA1) | (1<<PA0);
+
+	// Set PortD Pin 0 as Input
+	DDRD &= ~(1<<PD0);
+	// Set internal Pullups
+	PORTD |= (1<<PD0) | (1<<PD1);
+
+
+    // Set PD5 als input (Tristate)
+    DDRD &= ~(1 << PD5);
+    PORTD &= ~(1 << PD5);
+
+	// Clear LEDs
+	PORTA &= ~((1 << PA0) | (1 << PA1));
+
+
+	/* --- Init timer 0 (~10ms) that services leds  --- */
+	// 14745600 / 1024 = 14400 Hz
+	// 14400 / 100 = 144 Takte
+	// OCR0 = 143
+    TCCR0 =
+    (1<<WGM01) |   // CTC
+    (1<<CS02) |
+    (1<<CS00);     // /1024
+
+	OCR0 = 143;
+
+    TIMSK |= (1<<OCIE0);
 	
 
   	/*
@@ -344,14 +134,12 @@ int main (void)
      *  or 
      *  UART_BAUD_SELECT_DOUBLE_SPEED() ( double speed mode)
      */
-	Baudrate_COM1.ulBaudrate=DEFAULT_COM1_BAUDRATE;	
-    uart_init( UART_BAUD_SELECT(Baudrate_COM1.ulBaudrate,F_CPU) ); 
+    uart_init(UART_BAUD_SELECT(10400,F_CPU)); 
     
-	Baudrate_COM2.ulBaudrate=DEFAULT_COM2_BAUDRATE;	
-	uart1_init( UART_BAUD_SELECT(Baudrate_COM2.ulBaudrate,F_CPU) ); 
+	uart1_init(UART_BAUD_SELECT(19200,F_CPU)); 
 
 
-	// Enable all interrupts 
+	/* --- Enable interrupts --- */ 
     sei();
   
 
@@ -359,20 +147,21 @@ int main (void)
   	{
  
 		// Get current status of DTR line
-	    pa2 = (PINA&(1<<PA2));
+	    u8_dtr_value = (PINA&(1<<PA2));
 
-        // service PA2 status - DTR line
-        if(pa2!=lastState)
+        // Service PA2 status - DTR line
+        if(u8_dtr_value != u8_last_dtr_value)
         {
-            lastState=pa2;
+            u8_last_dtr_value = u8_dtr_value;
 
-            if(pa2)
+            if(u8_dtr_value)
             {
-                bridgeEnabled=1;
+                u8_bridgeEnabled=1;
                 
-				buffers_clear();
+				Clear_UART_Buffers();
 
-                UART0_Enable();
+                //Enable UART 0
+				UCSR0B = (1<<RXEN0)|(1<<TXEN0)|(1<<RXCIE0);
 				
                 // PD5 Tristate
                 DDRD &= ~(1<<PD5);
@@ -381,9 +170,10 @@ int main (void)
             }
             else
             {
-                bridgeEnabled=0;
+                u8_bridgeEnabled=0;
 
-                UART0_Disable();
+                // Disable UART 0
+				UCSR0B=0;
 
                 // PD5 LOW
                 PORTD &= ~(1<<PD5);
@@ -392,7 +182,7 @@ int main (void)
         }
 		
 		// Service UARTs only if bridge is not armed
-		if(bridgeEnabled == 1)
+		if(u8_bridgeEnabled == 1)
 		{
 			Service_UART0();
 			Service_UART1();
