b64d4d3046094e107d64876b86212163745cce80
ASiKS-Engineering <35295944+ASiKS-Engineering@users.noreply.github.com>
2026-05-27 12:30:47 +0200

    Update make_RS232KLineBridgeFirmware.bat

--- make_RS232KLineBridgeFirmware.bat
+++ make_RS232KLineBridgeFirmware.bat
@@ -5,8 +5,8 @@
 :: ATmega162
 :: --------------------------------------------------------
 
-avr-gcc.exe -mmcu=atmega162 -Wall -gdwarf-2 -std=gnu99 -DF_CPU=14745600 -O2 -fsigned-char -funsigned-bitfields -fpack-struct -fshort-enums -c main.c uart.c
-avr-gcc.exe -mmcu=atmega162 -Wl,-Map=uart_kline_bridge.map main.o uart.o -o uart_kline_bridge.elf
+avr-gcc.exe -mmcu=atmega162 -Wall -gdwarf-2 -std=gnu99 -DF_CPU=14745600 -O2 -fsigned-char -funsigned-bitfields -fpack-struct -fshort-enums -c main.c uart.c e2p.c
+avr-gcc.exe -mmcu=atmega162 -Wl,-Map=uart_kline_bridge.map main.o uart.o e2p.o -o uart_kline_bridge.elf
 avr-objcopy -O ihex -R .eeprom uart_kline_bridge.elf uart_kline_bridge_atmega162_v.hex
 
 srec_cat.exe uart_kline_bridge_atmega162_v.hex -Intel -Output uart_kline_bridge_atmega162_srec.hex -Intel -Line_Length 44
